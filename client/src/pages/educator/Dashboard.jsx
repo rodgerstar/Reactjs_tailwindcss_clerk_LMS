acd8a53023478861da8e6829a2788f8d4605d549
rodgers <rodgerstarrodgers@gmail.com>
2025-03-02 13:56:46 +0300

    StudentEnrolled

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -70,7 +70,7 @@ const Dashboard = () => {
                       {index + 1}
                     </td>
                     <td className='md:px-4 px-2 py-3 flex items-center space-x-3'>
-                      <img src={item.student.image_url} alt="profile"
+                      <img src={item.student.imageUrl} alt="profile"
                            className='w-9 h-9 rounded-full'/>
                       <span className='truncate'>{item.student.name}</span>
                     </td>
