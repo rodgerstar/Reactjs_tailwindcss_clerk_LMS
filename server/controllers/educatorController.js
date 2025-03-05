a52830d701e5954db0d8b1b4c98ef847642778df
rodgers <rodgerstarrodgers@gmail.com>
2025-03-05 15:47:14 +0300

    get course api build

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -53,3 +53,14 @@ export const addCourse = async (req, res)=>{
         res.json({success: false, message: error.message})
     }
 }
+
+// get educator courses
+export const getEducatorCourses = async (req, res)=>{
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+        res.json({success:true, courses})
+    }  catch (error){
+        res.json({success: false, message: error.message})
+    }
+}
--- server/routes/educatorRoute.js
+++ server/routes/educatorRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {addCourse, updateRoleToEducator} from "../controllers/educatorController.js";
+import {addCourse, getEducatorCourses, updateRoleToEducator} from "../controllers/educatorController.js";
 import upload from "../configs/multer.js";
 import {protectEducator} from "../middlewares/authMiddleware.js";
 
@@ -12,5 +12,6 @@ const educatorRouter = express.Router()
 educatorRouter.get('/update-role', updateRoleToEducator)
 educatorRouter.post('/add-course', upload.single('image'),
     protectEducator, addCourse)
+educatorRouter.get('/courses', protectEducator, getEducatorCourses)
 
 export default educatorRouter
